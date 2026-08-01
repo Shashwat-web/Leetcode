e869bd1f94337c9bdd85032e1ae9819c859923d8
Shashwat Tiwari <codex.shashwat@gmail.com>
2026-08-01 10:49:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (47.39%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,24 +1,26 @@
 class Solution:
     def maxProfit(self, prices):
         n = len(prices)
-        memo = {}
+        # memo = {}
+        dp = [[0,0] for _ in range (n + 2)]
 
-        def solve(i, flag):
-            if i >= n:
-                return 0
+            # if (i, flag) in memo:
+            #     return memo[(i, flag)]
 
-            if (i, flag) in memo:
-                return memo[(i, flag)]
+        for i in range(n-1, -1, -1) :
+            dp[i][1] = max(prices[i] + dp[i+2][0], dp[i+1][1])
+            dp[i][0] = max(-prices[i] + dp[i+1][1], dp[i+1][0])
+        return dp[0][0] 
 
-            if flag: 
-                sell = prices[i] + solve(i + 2, False)
-                hold = solve(i + 1, True)
-                memo[(i, flag)] = max(sell, hold)
-            else:     
-                buy = -prices[i] + solve(i + 1, True)
-                skip = solve(i + 1, False)
-                memo[(i, flag)] = max(buy, skip)
+        #     if flag: 
+        #         sell = prices[i] + solve(i + 2, False)
+        #         hold = solve(i + 1, True)
+        #         memo[(i, flag)] = max(sell, hold)
+        #     else:     
+        #         buy = -prices[i] + solve(i + 1, True)
+        #         skip = solve(i + 1, False)
+        #         memo[(i, flag)] = max(buy, skip)
 
-            return memo[(i, flag)]
+        #     return memo[(i, flag)]
 
-        return solve(0, False)
+        # return solve(0, False)
